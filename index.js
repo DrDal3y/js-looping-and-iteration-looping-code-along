4aaa08481745642aed4bf883722f9ae6fe37a25f
Marcus <marcus.daley1@outlook.com>
2021-04-04 17:50:14 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+// const namesArray = ['Lisa', 'Kaitlin', 'Jan']
+
+function writeCards(namesArray, surprise ) {
+    let thankYouCards = []
+    for (let i = 0; i < namesArray.length; i++) {
+        thankYouCards.push( `Thank you, ${namesArray[i]}, for the wonderful ${surprise} gift!` )
+    }
+    return thankYouCards
+}
+
+function countDown(startingNum = 10) {
+    while (startingNum > 0) {
+        console.log( startingNum )
+        startingNum -= 1
+    }
+    console.log(startingNum)
+}
